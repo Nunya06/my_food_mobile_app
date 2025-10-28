fdb392ad2471ee330bd3bad8d65c0d4e627a4dbd
Xorse Wisdom <wisdomxorse928@gmail.com>
2025-10-28 23:56:16 +0000

    my first commit

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,24 +1,43 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-import 'react-native-reanimated';
+import { SplashScreen, Stack } from "expo-router";
+import "./globals.css"
+import { useFonts } from 'expo-font'
+import { useEffect } from "react";
+import * as Sentry from '@sentry/react-native';
 
-import { useColorScheme } from '@/hooks/use-color-scheme';
+Sentry.init({
+  dsn: 'https://717c8029785f3c03de788f1d0086d1ee@o4510257725112320.ingest.de.sentry.io/4510269716627536',
 
-export const unstable_settings = {
-  anchor: '(tabs)',
-};
+  // Adds more context data to events (IP address, cookies, user, etc.)
+  // For more information, visit: https://docs.sentry.io/platforms/react-native/data-management/data-collected/
+  sendDefaultPii: true,
 
-export default function RootLayout() {
-  const colorScheme = useColorScheme();
+  // Enable Logs
+  enableLogs: true,
 
-  return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
-  );
-}
+  // Configure Session Replay
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1,
+  integrations: [Sentry.mobileReplayIntegration(), Sentry.feedbackIntegration()],
+
+  // uncomment the line below to enable Spotlight (https://spotlightjs.com)
+  // spotlight: __DEV__,
+});
+
+
+export default Sentry.wrap(function RootLayout() {
+  const [fontsLoaded, error] = useFonts({
+    "QuickSand-Bold": require('../assets/fonts/Quicksand-Bold.ttf'),
+    "QuickSand-Medium": require('../assets/fonts/Quicksand-Medium.ttf'),
+    "QuickSand-Regular": require('../assets/fonts/Quicksand-Regular.ttf'),
+    "QuickSand-SemiBold": require('../assets/fonts/Quicksand-SemiBold.ttf'),
+    "QuickSand-Light": require('../assets/fonts/Quicksand-Light.ttf'),
+  });
+
+  useEffect(() => {
+    if (error) throw error;
+    if (fontsLoaded) SplashScreen.hideAsync();
+  }, [fontsLoaded, error]);
+
+
+  return <Stack screenOptions={{ headerShown: false }} />;
+});
